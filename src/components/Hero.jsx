99b0d248cbec38dddd755e57890d07e6f40fdb8d
foysal-imitaz <foysalimtiaz86@gmail.com>
2025-01-10 18:24:54 +0600

    text size in sm devices fixed, projects button added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,8 +14,8 @@ const container = (delay) => ({
 
 const Hero = () => {
   return (
-    <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
-      <div className='flex flex-wrap justify-center items-center'>
+    <div className='border-b border-neutral-900 pb-4 flex items-center justify-center md:justify-center md:flex md:items-center lg:mb-35 sm:justify-center sm:flex sm:items-center'>
+     {/* <div className='flex flex-wrap justify-center items-center'>*/}
 
         {/* Hero Section Left Side */}
         <div className='w-full lg:w-1/2'>
@@ -24,18 +24,22 @@ const Hero = () => {
             variants={container(0)}
             initial="hidden"
             animate="visible"
-            className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl'>Foysal Imtiaz Sabab</motion.h1>
+  
+            className='text-center pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-7xl sm:text-5xl'
+
+            >Foysal Imtiaz Sabab</motion.h1>
             
             <motion.span 
             variants={container(0.5)}
             initial="hidden"
             animate="visible"
-            className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</motion.span>
+            className='text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent lg:text-4xl sm:text-4xl'
+            >Full Stack Developer</motion.span>
             <motion.p 
             variants={container(1)}
             initial="hidden"
             animate="visible"
-            className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+            className=' text-center my-2 max-w-xl py-6 font-light tracking-tighter'>
               {HERO_CONTENT}
             </motion.p>
           </div>
@@ -54,7 +58,7 @@ const Hero = () => {
           </div>
         </div>
         */}
-      </div>
+      
     </div>
   )  
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,41 +4,73 @@ import { motion } from 'framer-motion'
 
 const Projects = () => {
   return (
-    <div className='border-b border-neutral-900 pb-4'>
-      <motion.h1 
-      whileInView={{opacity: 1, y:0}}
-      initial={{opacity:0, y: -100}}
-      transition={{duration: 0.5}}
-      className='my-20 text-center text-4xl'>Projects</motion.h1>
+    <div className="border-b border-neutral-900 pb-4">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h1>
       <div>
-        {PROJECTS.map((project, index) =>(
-          <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-            <motion.div 
-              whileInView={{opacity: 1, x:0}}
-              initial={{opacity:0, x: -100}}
-              transition={{duration: 1}}
-              className='w-full lg:w-1/4'>
-              <img src={project.image} width={150} height={150} alt={project.title} 
-              className='mb-6 rounded'
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
+              <img
+                src={project.image}
+                width={150}
+                height={150}
+                alt={project.title}
+                className="mb-6 rounded"
               />
             </motion.div>
-            <motion.div 
-              whileInView={{opacity: 1, x:0}}
-              initial={{opacity:0, x: 100}}
-              transition={{duration: 1}}
-              className='w-full max-w-xl lg:w-3/4'>
-              <h6 className='mb-2 font-semibold'>{project.title}</h6>
-              <p className='mb-4 text-neutral-400'>{project.description}</p>
-              {project.technologies.map((tech, index) => (
-                <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
-              ))}
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
+              <h6 className="mb-2 font-semibold">{project.title}</h6>
+              <p className="mb-4 text-neutral-400">{project.description}</p>
+
+              {/* Code and Live Buttons */}
+              <a
+                href="#_" target='_blank'
+                class="relative inline-flex items-center justify-start inline-block px-4 py-1 overflow-hidden font-light text-sm rounded-full group"
+              >
+                <span class="w-22 h-22 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-gray-300 opacity-100 group-hover:-translate-x-8"></span>
+                <span class="relative w-full text-left text-gray-300 transition-colors duration-200 ease-in-out group-hover:text-black">
+                  Code
+                </span>
+                <span class="absolute inset-0 border border-gray-500 rounded-full"></span>
+              </a>
+              <span className='px-1'></span>
+              <a
+                href="#_" target='_blank'
+                class="relative inline-flex items-center justify-start inline-block px-4 py-1 overflow-hidden font-light text-sm rounded-full group"
+              >
+                <span class="w-22 h-22 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-gray-300 opacity-100 group-hover:-translate-x-8"></span>
+                <span class="relative w-full text-left text-gray-300 transition-colors duration-200 ease-in-out group-hover:text-black">
+                  Live
+                </span>
+                <span class="absolute inset-0 border border-gray-500 rounded-full"></span>
+              </a>
+
+
             </motion.div>
           </div>
         ))}
       </div>
     </div>
-
-  )
+  );
 }
 
 export default Projects
