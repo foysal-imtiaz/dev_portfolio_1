fd17acb784abc8259a5c110cfc2b2aed515bef6e
foysal-imitaz <foysalimtiaz86@gmail.com>
2025-01-10 13:17:06 +0600

    navbar button links added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from "../assets/kevinRushLogo.png"
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { FaSquareXTwitter } from 'react-icons/fa6'
@@ -22,9 +21,9 @@ const Navbar = () => {
 
 
     <div className='m-8 text-2xl flex items-center justify-center gap-4'>
-      <FaLinkedin />
-      <FaGithub />
-      <FaSquareXTwitter />
+      <a href="https://github.com/foysal-imtiaz" target='_blank' className='hover:text-gray-400'><FaGithub /></a>
+      <a href="https://www.linkedin.com/in/foysalimtiaz/" target='_blank' className='hover:text-gray-400'><FaLinkedin /></a>
+      <a href="https://x.com/foysal_imtiaz" target='_blank' className='hover:text-gray-400'><FaSquareXTwitter /></a>
     </div>
   </nav>
 }
